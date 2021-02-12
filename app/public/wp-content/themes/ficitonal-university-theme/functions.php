8920068b1db7ee551a13140cb3a5c1f22b604e52
N0matic <znaddaf@protonmail.com>
2021-02-12 13:53:26 -0600

    Updated theme and page links

--- app/public/wp-content/themes/ficitonal-university-theme/functions.php
+++ app/public/wp-content/themes/ficitonal-university-theme/functions.php
@@ -8,3 +8,9 @@
     }
 
     add_action('wp_enqueue_scripts', 'university_files');
+
+    function university_features(){
+        add_theme_support('title-tag');
+    }
+
+    add_action( 'after_setup_theme', 'university_features');
