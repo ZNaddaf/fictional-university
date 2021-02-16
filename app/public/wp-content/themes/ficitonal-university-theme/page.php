24cb68b2ee6a31a67e9781bb47ec9a444f97c5b2
N0matic <znaddaf@protonmail.com>
2021-02-16 12:15:25 -0600

    updated dynamic links

--- app/public/wp-content/themes/ficitonal-university-theme/page.php
+++ app/public/wp-content/themes/ficitonal-university-theme/page.php
@@ -15,25 +15,47 @@
 
   <div class="container container--narrow page-section">
 
+  <?php 
+    $theParent = wp_get_post_parent_id(get_the_ID());
+    if ($theParent) { ?>
     <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
+      <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
     </div>
+      
+    <?php }
+  ?> 
     
-    <!--
+    <?php
+    $testArray = get_pages(array(
+      'child_of' => get_the_ID()
+    ));
+
+    if ($theParent or $testArray) { ?>
     <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
+      <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
       <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
+        <?php
+        if ($theParent){
+          $findChildrenOf = $theParent;
+        } else {
+          $findChildrenOf = get_the_ID();
+        }
+
+        wp_list_pages(array(
+          'title_li' => NULL,
+          'child_of' => $findChildrenOf,
+          'sort_column' => 'menu_order'
+        ));
+        ?>
       </ul>
     </div>
-    -->
 
     <div class="generic-content">
       <?php the_content() ?>
     </div>
 
   </div>
+<?php } ?>
 
    <?php }
     get_footer();
